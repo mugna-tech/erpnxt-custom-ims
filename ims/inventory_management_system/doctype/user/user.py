bda180b902c929a95212a1ea2ea1be4e3ac10fb4
Alteyae <altheae.a.owe@gmail.com>
2024-07-14 03:42:20 +0800

    Workscpace for creating users

--- ims/inventory_management_system/doctype/user/user.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Mugna Tech and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class User(Document):
-	pass
