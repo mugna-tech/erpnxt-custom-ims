1a54c3b40ef9928fbb1868a7e7d480aacf843e77
Alteyae <altheae.a.owe@gmail.com>
2024-07-17 18:42:49 +0800

    Used frappe's random password generator

--- ims/inventory_management_system/doctype/ims_user/ims_user.js
+++ ims/inventory_management_system/doctype/ims_user/ims_user.js
@@ -1,23 +1,14 @@
-function generateRandomPassword(length) {
-    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    let password = "";
-    for (let i = 0; i < length; i++) {
-        password += charset[Math.floor(Math.random() * charset.length)];
-    }
-    return password;
-}
-
 frappe.ui.form.on('IMS User', {
     refresh(frm) {
         // If the password field is empty, generate a random password
         if (!frm.doc.password) {
-            frm.set_value('password', generateRandomPassword(12));
+            frm.set_value('password', frappe.utils.get_random(16));
         }
     },
     before_save(frm) {
         // If the password field is empty, generate a random password
         if (!frm.doc.password) {
-            frm.set_value('password', generateRandomPassword(12));
+            frm.set_value('password', frappe.utils.get_random(16));
         }
     }
 });
