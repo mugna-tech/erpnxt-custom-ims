1e6df81ca17257116cc34e97fd99befd0a6ecc5a
Alteyae <altheae.a.owe@gmail.com>
2024-07-17 17:48:20 +0800

    Remove unused code and randomPoz variable

--- ims/inventory_management_system/doctype/ims_user/ims_user.js
+++ ims/inventory_management_system/doctype/ims_user/ims_user.js
@@ -1,18 +1,8 @@
-// Copyright (c) 2024, Mugna Tech and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("IMS User", {
-// 	refresh(frm) {
-
-// 	},
-// });
-
 function generateRandomPassword(length) {
     let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     let password = "";
     for (let i = 0; i < length; i++) {
-        let randomPoz = Math.floor(Math.random() * charset.length);
-        password += charset.substring(randomPoz, randomPoz + 1);
+        password += charset[Math.floor(Math.random() * charset.length)];
     }
     return password;
 }
