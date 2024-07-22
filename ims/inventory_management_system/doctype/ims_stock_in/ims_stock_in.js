8b4e633f6c7d50cc31dfcc6daa2c5215d8436e8d
Alteyae <altheae.a.owe@gmail.com>
2024-07-22 13:18:25 +0800

    Added stock_in doctype extending Stock Entry doctype features

---
+++ ims/inventory_management_system/doctype/ims_stock_in/ims_stock_in.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Mugna Tech and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("IMS Stock In", {
+// 	refresh(frm) {
+
+// 	},
+// });
